4cb6ae3be2a466ccd0aeb3fd1df6c8f87e33597e
test <45917673+bettozzo@users.noreply.github.com>
2023-10-25 18:23:57 +0200

    Es 3

--- assignement_5/src/es3.rs
+++ assignement_5/src/es3.rs
@@ -1,3 +1,18 @@
+use std::fmt::{Debug, Display};
+
+fn restricted<T:PartialOrd + Debug,U:Display>(t1:T, t2:T, u:U){
+    print!("minor:\t");
+    if t1 < t2{
+        println!("{:?}", t1);
+    }else{
+        println!("{:?}", t2);
+    }
+    println!("u:\t{}", u);
+}
+
+
 pub fn main_es3(){
-    
+    restricted("ciao", "ciao_grande", "ciaoU");
+    println!();
+    restricted(6, 4, "Uè");
 }
