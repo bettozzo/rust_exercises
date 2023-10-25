f07e0b8d2b654d13fb477ce666c7cc9b331f00b1
test <45917673+bettozzo@users.noreply.github.com>
2023-10-25 18:33:28 +0200

    Half Es4

--- assignement_5/src/es4.rs
+++ assignement_5/src/es4.rs
@@ -1,3 +1,31 @@
+use std::fmt::Display;
+
+struct Tasks{
+    tasts:Vec<Task>
+}
+impl Tasks{
+    fn new() -> Self{
+        Self { tasts: Vec::new() }
+    }
+}
+
+struct Task{
+    name: String,
+    priority:i32,
+    done:bool,
+}
+impl Task{
+    fn new(name: String, priority:i32) -> Self{
+        Self { name, priority, done: false }
+    }
+}
+impl Display for Task{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Task  **{}** has priority {}. Status: {}", self.name, self.priority, self.done)
+    }
+}
+
+
 pub fn main_es4(){
     
 }
