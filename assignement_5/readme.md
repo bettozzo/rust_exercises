b201238a427ce55c0967250b164fc114ede46880
Bettozzo <45917673+bettozzo@users.noreply.github.com>
2023-10-25 13:46:04 +0200

    Init commit

---
+++ assignement_5/readme.md
@@ -0,0 +1,1 @@
+# [Click Here for the assignement](https://didatticaonline.unitn.it/dol/pluginfile.php/1780038/mod_resource/content/1/sheet5.pdf)
---
+++ assignement_5/src/main.rs
@@ -0,0 +1,24 @@
+pub mod es1;
+use es1::main_es1;
+pub mod es2;
+use es2::main_es2;
+pub mod es3;
+use es3::main_es3;
+pub mod es4;
+use es4::main_es4;
+pub mod es5;
+use es5::main_es5;
+pub mod es6;
+use es6::main_es6;
+pub mod es7;
+use es7::main_es7;
+
+fn main() {
+    main_es1();
+    main_es2();
+    main_es3();
+    main_es4();
+    main_es5();
+    main_es6();
+    main_es7();
+}
