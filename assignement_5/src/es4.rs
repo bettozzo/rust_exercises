d41245e9fcf01c14e831a20dd415f5a14d6a509f
Bettozzo <45917673+bettozzo@users.noreply.github.com>
2023-10-26 11:37:17 +0200

    Es 4

--- assignement_5/src/es4.rs
+++ assignement_5/src/es4.rs
@@ -1,22 +1,34 @@
-use std::fmt::Display;
 
+use std::{fmt::Display, path::Iter};
+
+#[derive(Debug)]
 struct Tasks{
-    tasts:Vec<Task>
+    tasks:Vec<Task>
 }
 impl Tasks{
     fn new() -> Self{
-        Self { tasts: Vec::new() }
+        Self { tasks: Vec::new() }
+    }
+}
+impl Iterator for Tasks{
+    type Item = Task;
+    fn next(&mut self) -> Option<Self::Item> {
+        return self.tasks.iter().position(|task| !task.done).map(|task| self.tasks.remove(task));
     }
 }
 
+#[derive(Debug, Clone)]
 struct Task{
     name: String,
     priority:i32,
     done:bool,
 }
 impl Task{
-    fn new(name: String, priority:i32) -> Self{
-        Self { name, priority, done: false }
+    fn new(name: &str, priority:i32) -> Self{
+        Self { name: name.to_string(), priority, done: false }
+    }
+    fn complete(&mut self){
+        self.done = true;
     }
 }
 impl Display for Task{
@@ -25,7 +37,20 @@ impl Display for Task{
     }
 }
 
-
+#[allow(unused_mut)]
 pub fn main_es4(){
-    
+    let mut tasks = Tasks::new();
+    let mut task1 = Task::new("task1", 10);
+    let mut task2 = Task::new("task2", 10);
+    let mut task3 = Task::new("task3", 10);
+    let mut task4 = Task::new("task4", 10);
+    let mut task5 = Task::new("task5", 10);
+    task1.complete();
+    task3.complete();
+    task5.complete();
+    tasks.tasks = vec![task1, task2, task3, task4, task5];
+    for ts in tasks{
+        print!("{:?}", ts);
+    }
+    // println!("{:?}", tasks);
 }
