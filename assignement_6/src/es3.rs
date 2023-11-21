389fd29e1008538eddae60f1913214c551d0a291
test <45917673+bettozzo@users.noreply.github.com>
2023-11-21 15:16:38 +0100

    Es 3

---
+++ assignement_6/src/es3.rs
@@ -0,0 +1,76 @@
+use std::{cell::RefCell, rc::Rc};
+
+trait Sound {
+    fn make_sound(&self) -> String;
+}
+
+#[derive(Clone, Debug)]
+struct Animals {
+    name: String,
+}
+impl Sound for Animals {
+    fn make_sound(&self) -> String {
+        format!("*{} SOUND*!", self.name)
+    }
+}
+
+#[derive(Debug)]
+struct FarmCell<S: Sound> {
+    element: S,
+    next: Option<Rc<RefCell<FarmCell<S>>>>,
+}
+
+impl FarmCell<Animals> {
+    fn new(obj: Animals) -> Self {
+        Self {
+            element: obj,
+            next: None,
+        }
+    }
+    fn insert(&mut self, obj: &Animals) {
+        // println!("{:?}", self);
+        match &self.next {
+            Some(next_node) => {
+                next_node.borrow_mut().insert(&obj);
+            }
+            None => {
+                self.next = Some(Rc::new(RefCell::new(Self {
+                    element: obj.to_owned(),
+                    next: None,
+                })))
+            }
+        }
+    }
+}
+impl Sound for FarmCell<Animals> {
+    fn make_sound(&self) -> String {
+        let mut output = "".to_string();
+        output.push_str(self.element.make_sound().as_str());
+        output.push('\n');
+
+        match &self.next {
+            Some(next_node) => {
+                output.push_str(next_node.borrow().make_sound().as_str());
+            }
+            None => (),
+        }
+        return output;
+    }
+}
+
+pub fn main_es3() {
+    let animal1: Animals = Animals {
+        name: "Ethan".to_string(),
+    };
+    let animal2 = Animals {
+        name: "Lola".to_string(),
+    };
+    let animal3 = Animals {
+        name: "Jessie".to_string(),
+    };
+    let mut farm = FarmCell::new(animal1);
+    farm.insert(&animal2);
+    farm.insert(&animal3);
+
+    println!("Listen to all of these sounds:\n{}", farm.make_sound());
+}
--- assignement_6/src/main.rs
+++ assignement_6/src/main.rs
@@ -5,11 +5,11 @@ use es1::main_es1;
 pub mod es2;
 use es2::main_es2;
 
-// pub mod es3;
-// use es3::main_es3;
+pub mod es3;
+use es3::main_es3;
 
 fn main() {
     // main_es1();
-    main_es2();
-    // main_es3();
+    // main_es2();
+    main_es3();
 }
